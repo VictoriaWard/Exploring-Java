1174e8ccb35310b1763a5df561110fa704aa87b4
Victoria Ward <vickylw_1@hotmail.com>
2014-05-27 18:26:24 +0200

    Updated salary calculator
    Updated output to display in money format (to 2dp)

--- Salary Calculator/src/salaryCalculator/Employee.java
+++ Salary Calculator/src/salaryCalculator/Employee.java
@@ -1,5 +1,8 @@
 package salaryCalculator;
 
+import java.io.*;
+import java.text.*;
+
 public class Employee {
 	//declare variables
 	double minimumPay = 8.0;
@@ -34,13 +37,16 @@ public class Employee {
 	double getMaxBaseHours(){
 			return maxBaseHours;
 		}
-				
+	
+    DecimalFormat moneyFormat = new DecimalFormat("0.00");
+
 	//method to calculate and display total pay for each employee by name			
 	void calculateTotalPay() {
 		if (basePay >= getMinPay() & numHoursWorked <= getMaxHours())
+			
 			if (numHoursWorked <= getMaxBaseHours())
-					System.out.println("Total pay for " + name + " is: $" + basePay * numHoursWorked);
-			else System.out.println("Total pay for " + name + " is: $" + ((basePay * getMaxBaseHours()) + (basePay * getOvertimeRate()) * (numHoursWorked - getMaxBaseHours())));
+				System.out.println("Total pay for " + name + " is: $" +  moneyFormat.format(basePay * numHoursWorked));
+			else System.out.println("Total pay for " + name + " is: $" +  moneyFormat.format(((basePay * getMaxBaseHours()) + (basePay * getOvertimeRate()) * (numHoursWorked - getMaxBaseHours()))));
 		else if (basePay <= getMinPay() & numHoursWorked >= getMaxHours())
 			System.out.println("The number of hours entered is too high and the base pay entered is too low for " + name);
 		else if (basePay <= getMinPay() & numHoursWorked <= getMaxHours())
